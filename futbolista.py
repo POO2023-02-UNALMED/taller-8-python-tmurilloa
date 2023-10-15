c371d7b7bb0450b99a87f1b9e5cf9f4e9abf13e8
tmurilloa <141962307+tmurilloa@users.noreply.github.com>
2023-10-14 22:23:46 -0500

    Update futbolista.py

--- futbolista.py
+++ futbolista.py
@@ -10,7 +10,7 @@ class Futbolista(Persona,Deportista):
         self._golesMarcados = golesMarcados
         self._tarjetasRojas = tarjetasRojas
         self._piernaHabil = piernaHabil
-        Futbolista._listadoFutbolista.append(self)
+        Futbolista._listaFutbolista.append(self)
 
     def getGolesMarcados(self):
         return self._golesMarcados
